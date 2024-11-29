cdb7479aa448fdcf9274706ba003b469f1346217
sonms78 <msson@kbfg.com>
2024-11-29 16:56:32 +0900

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,4 +1,14 @@
-module "s3-bucket" {
-  source  = "app.terraform.io/kb_sonms/s3-bucket/aws"
-  version = "4.2.2"
+module "s3_bucket_for_logs" {
+  source = "app.terraform.io/kb_sonms/s3-bucket/aws"
+
+  bucket = "my-s3-bucket-for-logs"
+  acl    = "log-delivery-write"
+
+  # Allow deletion of non-empty bucket
+  force_destroy = true
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  attach_elb_log_delivery_policy = true
 }
